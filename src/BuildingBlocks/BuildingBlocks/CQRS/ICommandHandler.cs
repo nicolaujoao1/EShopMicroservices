77b61345286e0eb3b886221403bc55d9aeb3e27d
Ilola Joao <ilolanicolau1999@gmail.com>
2024-07-17 12:02:27 +0100

    BuildingBlocks

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommandHandler.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS;
+
+public interface ICommandHandler<in TCommand, TResponse>:
+                        IRequestHandler<TCommand, TResponse>
+                        where TCommand : ICommand<TResponse>
+                        where TResponse:notnull
+{
+
+}
