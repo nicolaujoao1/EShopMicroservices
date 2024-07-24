c26bf8ec31c22d41dd6c334cb5cd74a3eeba6c93
Ilola Joao <ilolanicolau1999@gmail.com>
2024-07-24 15:40:09 +0100

    Customized Exception

--- src/Services/Catalog/Catalog.API/GlobalUsing.cs
+++ src/Services/Catalog/Catalog.API/GlobalUsing.cs
@@ -6,3 +6,4 @@ global using Catalog.API.Models;
 global using Marten;
 global using Catalog.API.Exceptions;
 global using FluentValidation;
+global using BuildingBlocks.Behaviors;
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,7 +1,4 @@
-using BuildingBlocks.Behaviors;
-using Microsoft.AspNetCore.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
-using static System.Net.Mime.MediaTypeNames;
+using BuildingBlocks.Exceptions.Handler;
 
 var builder = WebApplication.CreateBuilder();
 
@@ -20,37 +17,14 @@ builder.Services.AddMarten(opts =>
     opts.Connection(builder.Configuration.GetConnectionString("Database")!);
 }).UseLightweightSessions();
 
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
 
 var app = builder.Build();
 
 app.MapCarter();
 
-app.UseExceptionHandler(exceptionHandlerApp =>
-{
-    exceptionHandlerApp.Run(async context =>
-    {
-        var exception = context.Features.Get<IExceptionHandlerFeature>()?.Error;
-        if (exception is null)
-        {
-            return;
-        }
+app.UseExceptionHandler(options => { });  
 
-        var problemDetails = new ProblemDetails
-        {
-            Title = exception.Message,
-            Status = StatusCodes.Status500InternalServerError,
-            Detail = exception.StackTrace
-        };
-
-        var logger = context.RequestServices.GetRequiredService<ILogger<Program>>();
-        logger.LogError(exception, exception.Message);
-
-        context.Response.StatusCode = StatusCodes.Status500InternalServerError;
-        context.Response.ContentType= "application/problem+json";   
-
-
-        await context.Response.WriteAsJsonAsync(problemDetails);
-    });
-});
 
 app.Run();
