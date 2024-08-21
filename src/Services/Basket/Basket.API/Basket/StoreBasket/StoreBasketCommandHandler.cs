976e5174edf107f2642f29a35506db5fd79a6d53
Ilola Joao <ilolanicolau1999@gmail.com>
2024-08-21 14:47:51 +0100

    Alteracoes

--- src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketCommandHandler.cs
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketCommandHandler.cs
@@ -1,8 +1,10 @@
 ﻿
 
+using Discount.Grpc;
+
 namespace Basket.API.Basket.StoreBasket;
 
-public record StoreBasketCommand(ShoppingCart Cart):ICommand<StoreBasketResult>;
+public record StoreBasketCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
 
 public record StoreBasketResult(string UserName);
 
@@ -15,12 +17,24 @@ public class StoreBasketCommandValidator : AbstractValidator<StoreBasketCommand>
     }
 }
 
-public class StoreBasketHandler(IBasketRespository respository) : ICommandHandler<StoreBasketCommand, StoreBasketResult>
+public class StoreBasketHandler(IBasketRespository respository, DiscountProtoService.DiscountProtoServiceClient discountProto) : ICommandHandler<StoreBasketCommand, StoreBasketResult>
 {
     public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
     {
+
+        await DeductDiscount(command.Cart, cancellationToken);
+
         await respository.StoreBasketAsync(command.Cart, cancellationToken);
 
         return new StoreBasketResult(command.Cart.UserName);
+    }
+    private async Task DeductDiscount(ShoppingCart cart, CancellationToken cancellationToken)
+    {
+        foreach (var item in cart.Items)
+        {
+            var coupon = await discountProto.GetDiscountAsync(new GetDiscountRequest { ProductName = item.ProductName }, cancellationToken: cancellationToken);
+
+            item.Price -= coupon.Amount;
+        }
     }
 }
--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,9 +1,12 @@
 using BuildingBlocks.Exceptions.Handler;
+using Discount.Grpc;
 using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
+//Application Services
 builder.Services.AddCarter();
 builder.Services.AddMediatR(conf =>
 {
@@ -15,6 +18,7 @@ builder.Services.AddMediatR(conf =>
 var connectionStringPostsGres = builder.Configuration.GetConnectionString("Database")!;
 var connectionStringRedis = builder.Configuration.GetConnectionString("Redis")!;
 
+//Data Services
 builder.Services.AddMarten(opts =>
 {
     opts.Connection(connectionStringPostsGres);
@@ -27,6 +31,21 @@ builder.Services.Decorate<IBasketRespository, CachedBasketRepository>();
 builder.Services.AddStackExchangeRedisCache(options =>
 {
     options.Configuration = connectionStringRedis;
+});
+
+
+//Grpc Services
+builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(options =>
+{
+    options.Address = new Uri(builder.Configuration["GrpcSettings:DiscountUrl"]!);
+}).ConfigurePrimaryHttpMessageHandler(() =>
+{
+
+    var handler = new HttpClientHandler
+    {
+        ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+    };
+    return handler;
 });
 
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
@@ -42,7 +61,7 @@ app.MapCarter();
 app.UseExceptionHandler(options => { });
 app.UseHealthChecks("/health", new HealthCheckOptions
 {
-    ResponseWriter=UIResponseWriter.WriteHealthCheckUIResponse
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
 });
 
 app.Run();
